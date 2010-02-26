a97744f8a1f027e2bc53164328f54c0d55749349
Adrian Bartol <adri24an@gmail.com>
2010-02-26 10:17:50 +0100

    añadido metodo AddInstance

--- src/Parser.h
+++ src/Parser.h
@@ -44,6 +44,18 @@ class Parser: public ParserBase
 
         int parse();
 
+	//Adds a new instance (because vesions 2 and 3 of AddInstance had 
+	//the same parameters, we introduce the parameter "aux" in the third
+	//version of AddInstance so the compiler can differentiate between
+	//them)
+	void AddInstance(std::string iid, std::string tid, std::string name);
+	void AddInstance(std::string iid, std::string tid);
+	void AddInstance(std::string iid, std::string name, int aux);
+	void AddInstance(std::string iid);
+	
+	//Adds a new message
+	void AddMessage();
+
 	//A custom print operation
 	void MyPrint();       
 
