5e94af063832b72cfaf037049a8884059d620202
Adrian Bartol <adri24an@gmail.com>
2010-03-07 17:03:52 +0100

    corregido error en addMsg (no se puso tipo salida)

--- src/Messages.cpp
+++ src/Messages.cpp
@@ -4,11 +4,12 @@ Messages :: Messages()
 {
 }
 
-Messages :: addMsg(int id, char * new_mid, char * new_sms, char * new_origin,
-	      char * new_destiny, int new_time_sent, int new_time_rec)
+void Messages :: addMsg(int id, char * new_mid, char * new_sms, 
+			char * new_origin, char * new_destiny, 
+			int new_time_sent, int new_time_rec)
 {
-  ASTmessages * m = new ASTmessages(id, new_mid, new_sms, new_origin, 
+  ASTmessages * m = new ASTmessages(new_mid, new_sms, new_origin, 
 				    new_destiny, new_time_sent, new_time_rec);
-    astInst[id] = m;
+  astInst[id] = m;
 }
 
