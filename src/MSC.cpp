954ffbdc74d39043854d6b4ba68116b9f6db6c68
Adrian Bartol <adri24an@gmail.com>
2010-03-07 17:01:21 +0100

    añadido metodo addMsg

--- src/MSC.cpp
+++ src/MSC.cpp
@@ -9,3 +9,10 @@ void MSC :: addInst (char * newiid, char * newtid, char * name)
 {
   instances->addInst (newiid, newtid, name);
 }
+
+void MSC :: addMsg (int id, char * new_mid, char * new_sms, char * new_origin, 
+	      char * new_destiny, int new_time_sent, int new_time_rec)
+{
+  messages->addMsg (id, new_mid, new_sms, new_origin, new_destiny, 
+		    new_time_sent, new_time_rec);
+}
