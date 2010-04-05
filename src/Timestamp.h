7ce9793021da654a6ae2cd8b9043f118f75baa9b
Adrian Bartol <adri24an@gmail.com>
2010-04-05 16:27:03 +0200

    añadidos nuevos comentarios al codigo

--- src/Timestamp.h
+++ src/Timestamp.h
@@ -14,10 +14,16 @@ public:
 
   Timestamp(char * new_iid, Timeref * new_time);
   ~Timestamp();
+
+  //Returns iid
   char * getIid ();
+  //Returns the atribute Valtype of time object
   int getValtype ();
+  //Returns the atribute Value of time object
   int getValue ();
+  //Sets iid value
   void setIid (char * id);
+  //Sets Valtype and Value values of time object
   void setTime (int valtype, int value);
 
 private:
