f0f1542e0204fce95c76ae0531ef777aa9a93ecb
Adrian Bartol <adri24an@gmail.com>
2010-04-05 16:13:01 +0200

    deleted default constructor and added new comment to code

--- src/ASTmessages.h
+++ src/ASTmessages.h
@@ -8,23 +8,33 @@ class ASTmessages
   
 public:
   
-  ASTmessages();
   ASTmessages(char * new_mid, char * new_sms, char * new_origin, 
 	      char * new_destiny, int new_time_sent, int new_time_rec);
   ~ASTmessages();
 
+  //Returns the mid of the message
   char * getMid();
+  //Returns the sms of the message
   char * getSms();
+  //Returns the origin of the message
   char * getOrigin();
+  //Returns the destiny of the message
   char * getDestiny();
+  //Returns the time when the message was sent
   int getTime_sent();
+  //Returns the time when the message was received
   int getTime_rec();
-
+  //Sets the value of mid
   void setMid(char * id);
+  //Sets the value of sms
   void setSms(char * id);
+  //Sets the value of origin
   void setOrigin(char * id);
+  //Sets the value of destiny
   void setDestiny(char * id);
+  //Sets the value of time_sent
   void setTime_sent(int id);
+  //Sets the value of time_rec
   void setTime_rec(int id);
 
 private:
