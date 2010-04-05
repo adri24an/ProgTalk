32ac4e1865d04b60ec09f559f9b0943ff924d915
Adrian Bartol <adri24an@gmail.com>
2010-04-05 16:14:52 +0200

    added new comments to the code

--- src/Messages.h
+++ src/Messages.h
@@ -17,8 +17,11 @@ public:
   //Adds a new message
   void addMsg(char * new_mid, char * new_sms, char * new_origin,
 	      char * new_destiny, int new_time_sent, int new_time_rec);
+  //Returns the time when the message was sent
   int getTime_sent(int id);
+  //Returns the time when the message was received
   int getTime_rec(int id);
+  //Returns the number of messages storaged
   int msgSize();
   
 private:
