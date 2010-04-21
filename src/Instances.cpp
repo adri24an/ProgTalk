314653c15862bb9d8e34734a3091492a999e32be
Adrian Bartol <adri24an@gmail.com>
2010-04-21 16:37:37 +0200

    corregido bug por el cual, podiamos en ciertas condiciones almacenar sin error varias instancias con igual iid

--- src/Instances.cpp
+++ src/Instances.cpp
@@ -11,18 +11,22 @@ Instances :: ~Instances()
 
 void Instances :: addInst(char * new_iid, char * new_tid, char * new_name)
 { 
-    ASTinstances * i;
+  ASTinstances * i;
+  std::map<char *, ASTinstances *>::const_iterator
+    mit (astInst.begin()),
+    mend(astInst.end());
 
-    if(astInst.find(new_iid) == astInst.end())
-      {	
-		i = new ASTinstances(new_tid, new_name);
-		astInst[new_iid] = i;
-      }
-    else
-      {
-		std::cout << "Error: The instance already exists." << std::endl;
-		exit(0);
-      }
+  for(;mit!=mend;++mit) 
+	{
+	  if (!(strcmp(mit->first, new_iid)))
+		{	
+		  std::cout << "Error: The instance already exists." << std::endl;
+		  exit(0); 
+		}
+	}
+  
+  i = new ASTinstances(new_tid, new_name);
+  astInst[new_iid] = i;
 }
 
 void Instances :: myPrint()
