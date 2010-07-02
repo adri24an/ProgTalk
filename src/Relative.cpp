5961b3a1fde8f6d6458b1239efd1b5086c265229
Adrian Bartol <adri24an@gmail.com>
2010-07-02 19:24:05 +0200

    implentado constructor y parte de getAbsoluteTime

--- src/Relative.cpp
+++ src/Relative.cpp
@@ -2,9 +2,17 @@
 #include "Relative.h"
 #include "Event.h"
 
-Relative::Relative() {
+Relative::Relative() 
+{
+  ref = NULL;
+  diff = 0;
 }
 
 Relative::~Relative() {
+}
+
+int Relative::getAbsoluteTime()
+{
+  int ref_val = getAbsoluteTime; 
 }
 
