2ed40bfa917ca06eadb74958f8a9832ccfbb8f46
Adrian Bartol <adri24an@gmail.com>
2010-07-07 17:47:54 +0200

    implementado metodo addMsg de MSC

---
+++ doc/diagramas/addMsg_body_a2626d0_2.cpp
@@ -0,0 +1,20 @@
+std::map<int, Message *>::const_iterator
+  mit (messages.begin()),
+  mend(messages.end());
+int id = 0;
+
+if ((strcmp (m->mid, "No_Info_Available")) != 0)
+  {
+    for(;mit!=mend;++mit) 
+      {
+	if ((strcmp(mit->second->mid, m->mid)) == 0)
+	  {
+	    std::cout << "Error: the mid (" << m->mid << 
+	      ") used in the message already exists" << std::endl;
+	    exit(0);
+	  }
+      }
+  }
+
+id = (int) messages.size();
+messages[id] = m;
