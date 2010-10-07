a736a2ae39e414c52854b7b34d3856fd873f561a
Adrian Bartol <adri24an@gmail.com>
2010-10-07 17:06:21 +0200

    testing

--- src/Progtalk.cpp
+++ src/Progtalk.cpp
@@ -1,5 +1,10 @@
 //#include "Parser.h"
+#include "Absolute.h"
+#include "Relative.h"
+#include "Sending.h"
+#include "Receipt.h"
 #include "Instance.h"
+#include "Message.h"
 #include "MSC.h"
 
 int main()
@@ -8,15 +13,24 @@ int main()
 
   //parser->parse();
 
-  Instance i1 ("sms1", "type1", "comment");
-  Instance i2 ("sms2", "type2", "comment number two");
-  Instance i3 ("sms3", "type3", "comment number three");
-  Message m1();
+  Instance i1 ("id1", "type1", "comment");
+  Instance i2 ("id2", "type2", "comment number two");
+  Instance i3 ("id3", "type3", "comment number three");
+
+  Absolute a1 (1);
+  Sending s1 (i1, a1);
+
+  Relative re1 (2, s1);
+  Receipt r1 (i2, re1);
+
+  Message m1("m1", "sms1", s1, r1);
+
   MSC * msc = new MSC;
 
   msc->addInst(i1);
   msc->addInst(i2);
   msc->addInst(i3);
+  msc->addMsg(m1);
   msc->print();
   
   return 0;
