f916f1f7c6f2bb9e6f610684980102dc0328faec
Adrian Bartol <adri24an@gmail.com>
2010-10-07 14:03:30 +0200

    inicio de pruebas. vamos modificando progtalk para ir probando cada modulo. luego guardaremos estas pruebas en ./Pruebas y Progtalk.cpp volvera a ser como antes de las pruebas.

--- src/Progtalk.cpp
+++ src/Progtalk.cpp
@@ -1,10 +1,17 @@
-#include "Parser.h"
+//#include "Parser.h"
+#include "Instance.h"
+#include "MSC.h"
 
 int main()
 {
-  Parser * parser = new Parser;
+  //Parser * parser = new Parser;
 
-  parser->parse();
+  //parser->parse();
+
+  Instance i ("sms1", "type1", "comment");
+  MSC * msc = new MSC;
+  msc->addInst(i);
+  msc->print();
   
   return 0;
 }
