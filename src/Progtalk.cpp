a2e4c53219e33456b33580b34ca239d0847f5319
Adrian Bartol <adri24an@gmail.com>
2010-10-07 16:32:27 +0200

    testing

--- src/Progtalk.cpp
+++ src/Progtalk.cpp
@@ -8,9 +8,15 @@ int main()
 
   //parser->parse();
 
-  Instance i ("sms1", "type1", "comment");
+  Instance i1 ("sms1", "type1", "comment");
+  Instance i2 ("sms2", "type2", "comment number two");
+  Instance i3 ("sms3", "type3", "comment number three");
+  Message m1();
   MSC * msc = new MSC;
-  msc->addInst(i);
+
+  msc->addInst(i1);
+  msc->addInst(i2);
+  msc->addInst(i3);
   msc->print();
   
   return 0;
