72212a63bd179801033a3045d298b0eaa9045fec
Adrian Bartol <adri24an@gmail.com>
2010-11-08 23:55:36 +0100

    implementados todos los metodos

--- src/LatexPrint.cpp
+++ src/LatexPrint.cpp
@@ -2,6 +2,8 @@
 
 LatexPrint::LatexPrint() {
   ofstream fs("communication.msc");
+  msg = new string;
+  pair<int,int> aux (-1,-1);
   
   fs << "\\documentclass{article}\n";
   fs << "\\usepackage{msc}\n";
@@ -11,6 +13,7 @@ LatexPrint::LatexPrint() {
 }
 
 LatexPrint::~LatexPrint() {
+  delete msg;
 }
 
 void LatexPrint::visitMSC(MSC * m)
@@ -27,10 +30,21 @@ void LatexPrint::visitInstance(Instance * i)
 
 void LatexPrint::visitInstanceEvent(Instance * i)
 {
+  std::stringstream stream;
+
+  stream << "{" << i->get_iid() << "}";
+  msg->append(stream.str());
 }
 
 void LatexPrint::visitMessage(Message * m)
 {
+  fs.open("communication.msc");
+  fs << "\\mess{" << m->get_sms() << "}" << *msg;
+  fs << "\\mscmark{t=" << aux.first << "}{" 
+     << m->get_sending()->get_instance() << "}\n";
+  fs << "\\nextlevel\n";
+  fs << "\\mscmark{t=" << aux.second << "}{" 
+     << m->get_receipt()->get_instance() << "}\n";
 }
 
 void LatexPrint::visitSending(Sending * s)
@@ -43,8 +57,16 @@ void LatexPrint::visitReceipt(Receipt * r)
 
 void LatexPrint::visitAbsolute(Absolute * a)
 {
+  if (aux.first == -1)
+    aux.first = a->getAbsoluteTime();
+  else
+    aux.second = a->getAbsoluteTime();
 }
 
 void LatexPrint::visitRelative(Relative * r)
 {
+  if (aux.first == -1)
+    aux.first = r->getAbsoluteTime();
+  else
+    aux.second = r->getAbsoluteTime();
 }
