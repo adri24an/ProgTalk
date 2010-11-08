c8fed81998e7331f246d1e2e23d3634dce5ae4cd
Adrian Bartol <adri24an@gmail.com>
2010-11-08 20:01:30 +0100

    first commit

---
+++ src/LatexPrint.h
@@ -0,0 +1,34 @@
+#ifndef _LATEXPRINT_H
+#define _LATEXPRINT_H
+
+#include "Absolute.h"
+#include "Relative.h"
+#include "Sending.h"
+#include "Instance.h"
+#include "Message.h"
+#include "MSC.h"
+#include "MSCVisitor.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+class LatexPrint : public MSCVisitor {
+
+ public:
+
+  LatexPrint();
+  ~LatexPrint();
+  void visitMSC(MSC * m);
+  void visitInstance(Instance * i);
+  void visitInstanceEvent(Instance * i);
+  void visitMessage(Message * m);
+  void visitSending(Sending * s);
+  void visitReceipt(Receipt * r);
+  void visitAbsolute(Absolute * a);
+  void visitRelative(Relative * r);
+
+ private:
+  
+  ofstream * fs;
+};
+#endif 
