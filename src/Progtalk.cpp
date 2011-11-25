e5c3bbe5fb270e25387220a318219235fce1d3eb
Adrian Bartol <adri24an@gmail.com>
2011-11-25 20:35:12 +0100

    correcion final

--- src/Progtalk.cpp
+++ src/Progtalk.cpp
@@ -8,12 +8,12 @@ int main()
   Parser * parser = new Parser;
   PrettyPrinting * visitorpprinting = new PrettyPrinting;
   TimeChecker * visitortime = new TimeChecker;
-  //LatexPrint * visitorprint = new LatexPrint;
+  LatexPrint * visitorprint = new LatexPrint;
 
   parser->parse();
   parser->accept(*visitortime);
-
-  parser->accept(*visitorpprinting);
+  parser->accept(*visitorpprinting); // Esto lo podemos quitar cuando queramos.
+  parser->accept(*visitorprint);
   
   //parser->accept(*visitorprint);
   
